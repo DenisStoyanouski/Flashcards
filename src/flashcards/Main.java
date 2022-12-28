95d6be51714dd894083aa718b2126470a5cd39a6
Denis Stoyanouski <vandenst@tut.by>
2022-12-28 23:15:56 +0300

    add methods getNumber, createCards, checkCards

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -7,13 +7,47 @@ public class Main {
 
     static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("Input the number of cards:");
-        int number = Integer.parseInt(input());
+    static int number = 0;
 
+    static String[][] cards;
+
+    public static void main(String[] args) {
+        getNumber();
+        createCards();
+        checkCards();
+    }
+
+    private static void getNumber() {
+        System.out.println("Input the number of cards:");
+        number = Integer.parseInt(input());
     }
 
     private static String input() {
         return scanner.nextLine();
+    }
+
+    private static void createCards() {
+
+        cards = new String[number][2];
+        int count = 1;
+        for (String[] card : cards) {
+            System.out.printf("Card #%d%n", count);
+            card[0] = input();
+            System.out.printf("The definition for card #%d:%n", count);
+            card[1] = input();
+            count++;
+        }
+    }
+
+    private static void checkCards() {
+        for (String[] card : cards) {
+            System.out.printf("Print the definition of \"%s\":%n", card[0]);
+            String answer = input();
+            if (Objects.equals(answer, card[1])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.printf("Wrong. The right answer is \"%s\"%n", card[1]);
+            }
+        }
     }
 }
