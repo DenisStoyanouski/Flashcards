c82eb390d9bcc00d8436b8a69655cb52fdbf7530
Denis Stoyanouski <vandenst@tut.by>
2022-12-31 22:53:19 +0300

    add LinkedHashMap and redone methods

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,7 +1,6 @@
 package flashcards;
 
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -9,17 +8,18 @@ public class Main {
 
     static int number = 0;
 
-    static String[][] cards;
+    static Map<String, String> cards = new LinkedHashMap<>();
 
     public static void main(String[] args) {
         getNumber();
-        createCards();
-        checkCards();
+
+
     }
 
     private static void getNumber() {
         System.out.println("Input the number of cards:");
         number = Integer.parseInt(input());
+        createCards();
     }
 
     private static String input() {
@@ -27,26 +27,49 @@ public class Main {
     }
 
     private static void createCards() {
-
-        cards = new String[number][2];
         int count = 1;
-        for (String[] card : cards) {
+        String term = null;
+        String definition = null;
+        while (number != 0) {
             System.out.printf("Card #%d%n", count);
-            card[0] = input();
+            do {
+                term = input();
+                if (cards.containsKey(term)) {
+                    System.out.printf("The term \"%s\" already exists. Try again:%n", term);
+                }
+            } while (cards.containsKey(term));
+
             System.out.printf("The definition for card #%d:%n", count);
-            card[1] = input();
+            do {
+                definition = input();
+                if (cards.containsValue(definition)) {
+                    System.out.printf("The definition \"%s\" already exists. Try again:%n", definition);
+                }
+            } while (cards.containsValue(definition));
+            cards.put(term, definition);
             count++;
+            number--;
         }
+        checkCards();
     }
 
     private static void checkCards() {
-        for (String[] card : cards) {
-            System.out.printf("Print the definition of \"%s\":%n", card[0]);
+        for (String term : cards.keySet()) {
+            System.out.printf("Print the definition of \"%s\":%n", term);
             String answer = input();
-            if (Objects.equals(answer, card[1])) {
+            if (Objects.equals(answer, cards.get(term))) {
                 System.out.println("Correct!");
+            } else if (cards.containsValue(answer)) {
+                String termSecond = null;
+                for (var entry : cards.entrySet()) {
+                    if (Objects.equals(entry.getValue(), answer)) {
+                        termSecond = entry.getKey();
+                    }
+                }
+                System.out.printf("Wrong. The right answer is \"%s\", but your definition is correct " +
+                        "for \"%s\"%n", cards.get(term), termSecond);
             } else {
-                System.out.printf("Wrong. The right answer is \"%s\"%n", card[1]);
+                System.out.printf("Wrong. The right answer is \"%s\"%n", cards.get(term));
             }
         }
     }
